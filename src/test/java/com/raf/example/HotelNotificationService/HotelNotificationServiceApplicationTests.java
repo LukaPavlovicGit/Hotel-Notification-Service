15e3f1ecc55228dd332fcfe2ab020150ce6a1fd6
lukapavlovic <lpavlovic11521rn@raf.rs>
2022-09-04 02:34:12 +0200

    Initial commit

---
+++ src/test/java/com/raf/example/HotelNotificationService/HotelNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.example.HotelNotificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
